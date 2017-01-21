8d021b44114d5c5fd9646f4ba8ec0f767d804404
frazeal <aaalves.francisco@gmail.com>
2017-01-21 15:25:39 -0200

    Add files via upload

---
+++ Banco/BancoLibrary/ContaCorrente.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ErrosLibrary;
+
+namespace BancoLibrary
+{
+    public class ContaCorrente : Conta, ITributavel
+    {
+
+        public override string TipoConta
+        {
+            get
+            {
+                return "Conta Corrente";
+            }
+        }
+        
+        //public override bool Sacar(double valor)
+        //{
+        //    if (this.Saldo >= valor)
+        //    {
+        //        this.Saldo -= valor;
+        //        return true;
+        //    }
+        //    return false;
+        //}
+        public override void Sacar(double valor)
+        {
+            if (this.Saldo >= valor)
+            {
+                this.Saldo -= valor;
+            }
+            else
+            {
+                throw new SaldoInsuficienteException();
+                //throw new Exception("O valor do saque excede o valor do saldo disponível.");             
+            }
+        }
+
+        public override void Depositar(double valor)
+        {
+            this.Saldo += valor;
+        }
+
+        public double CalculaTributos()
+        {
+            return this.Saldo * 0.05;
+        }
+    }
+}
---
+++ Banco/BancoLibrary/ContaPoupanca.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ErrosLibrary;
+
+namespace BancoLibrary
+{
+    public class ContaPoupanca: Conta
+    {
+        public ContaPoupanca() : base()
+        {
+                
+        }
+
+        //public ContaPoupanca(double saldoInicial) : base(saldoInicial)
+        //{
+
+        //}
+
+        public override string TipoConta 
+        { 
+            get
+            {
+                return "Conta Poupança";
+            }
+        }
+
+        //public override void Sacar(double valor)
+        //{
+        //     if (this.Saldo >= valor)
+        //    {
+        //        this.Saldo -= (valor + 1);
+        //        return true;
+        //    }
+        //    return false;
+        //}
+
+        public override void Sacar(double valor)
+        {
+            if ((valor + 0.1) > this.Saldo) 
+            {
+                //throw new Exception("O valor do saque excede o valor do saldo disponível.");
+                throw new SaldoInsuficienteException();
+            }
+            else
+            {
+                this.Saldo -= (valor + 0.1);
+            }
+        }
+
+        public override void Depositar(double valor)
+        {
+            if (valor < 0.0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                this.Saldo += valor;
+            }
+        }
+
+    }
+}
