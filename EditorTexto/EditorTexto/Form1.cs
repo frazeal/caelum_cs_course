75b70ff9920d7fb1b29c0e4161ee51cdc9a84be3
frazeal <aaalves.francisco@gmail.com>
2017-02-04 15:20:56 -0200

    Add files via upload

---
+++ EditorTexto/EditorTexto/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EditorTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            File.Delete(@"C:\Users\csharp6472\Documents\Visual Studio 2013\Projects\EditorTexto\texto.txt");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            txtEditor.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(@"C:\Users\csharp6472\Documents\Visual Studio 2013\Projects\EditorTexto\texto.txt", txtEditor.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtEditor.Text = File.ReadAllText(@"C:\Users\csharp6472\Documents\Visual Studio 2013\Projects\EditorTexto\texto.txt");
+        }
+    }
+}
---
+++ EditorTexto/Simulador/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Simulador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> actors = new List<string>()
+            {
+                "Mark Hamill",
+                "Harrison Ford",
+                "Carrie Fisher"
+            };
+
+            // Your code here.
+            var file = new FileStream(@".\Movies.txt", FileMode.Create);
+            var writer = new StreamWriter(file);
+
+            foreach (var actor in actors)
+            {
+                writer.WriteLine(actor);
+            }
+
+            writer.Close();
+
+            string str = "[message details in here][another message here]/n/n[anothermessage here]";
+            string[] result = Regex.Matches(str, @"\[.*?\]").Cast<Match>().Select(m => m.Value).ToArray();
+
+            foreach (var r in result)
+            {
+                Console.WriteLine(r.ToString());
+            }
+
+            List<string> actors2 = new List<string>()
+        {
+            "Mark Hamill",
+            "Harrison Ford",
+            "Carrie Fisher"
+        };
+
+            // Your code here.
+            using (var file2 = new FileStream(@".\Actors.txt", FileMode.Create))
+            using (var writer2 = new StreamWriter(file2))
+            {
+                foreach (string actor in actors2)
+                {
+                    writer2.WriteLine(actor);
+                    }
+                }
+
+            Console.ReadKey();
+
+            Directory.CreateDirectory(@".\sub-dir1");
+            Directory.CreateDirectory(@".\sub-dir2");
+            Directory.CreateDirectory(@".\sub-dir3");
+
+            // Your code here
+            DirectoryInfo directory = new DirectoryInfo(@".\");
+            var children = directory.GetDirectories();
+
+            foreach (var child in children)
+            {
+                Console.WriteLine(child.Name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
